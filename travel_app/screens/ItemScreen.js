53dfbbe2488c334f94469fbf593766c5a7db8ac3
ruchikageeminda97 <ruchikarg97@gmail.com>
2023-06-28 23:17:06 +0530

    main item screen

--- travel_app/screens/ItemScreen.js
+++ travel_app/screens/ItemScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const ItemScreen = ({route}) => {
     const navigation= useNavigation();
@@ -29,7 +30,46 @@ return (
                 }
             className="w-full h-72 object-cover rounded-2xl"
             />
+
+            <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
+               
+                <TouchableOpacity 
+                    onPress={() => navigation.navigate("Discover")}
+
+                    className="w-10 h-10 rounded-md items-center justify-center bg-[#fdffff]">
+                     <FontAwesome5 name="chevron-left" size={24} color="#06B2BE" />
+                </TouchableOpacity>
+
+                <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#06B2BE]">
+                         <FontAwesome5 name="heartbeat" size={24} color="#fff" />
+                </TouchableOpacity>
+
+            </View>
+
+
+            <View className="absolute flex-row inset-x-0 bottom-5 justify-between px-6">
+               
+               <View className="flex-row space-x-2 items-center">
+                    <Text className="text-[12px] font-bold text-gray-100 ">
+                        {data?.price_level}
+                    </Text>
+
+                    <Text className="text-[32px] font-bold text-gray-100">
+                        {data?.price}
+                    </Text>
+               </View>
+
+                <View>
+                <Text className="px-2 py-1 rounded-md bg-teal-600 text-[#fff]">
+                        {data?.open_now_text}
+                    </Text>
+                </View>
+
+            </View>
+
         </View>
+
+
     </ScrollView>
         
    </SafeAreaView>
